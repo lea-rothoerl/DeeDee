57720c86f3f13e4e810e0672385ff2e057ff769c
Lea Rothoerl <lea.rothoerl@th-bingen.de>
2021-06-12 09:43:18 +0200

    remove logFC coloring options

--- R/deedee_qq.R
+++ R/deedee_qq.R
@@ -8,7 +8,7 @@
 #' @param select1 index of first data-list element to be used (default = 1)
 #' @param select2 index of second data-list element to be used (default = 2)
 #' @param color_by indicates which set of values the output should be colored by
-#'                 (possible values = "logFC1", "logFC2", "pval1" (default),
+#'                 (possible values = "pval1" (default),
 #'                 "pval2")
 #'
 #' @return ggplot object (plottable with show()/print())
@@ -32,14 +32,13 @@ deedee_qq <- function(data,
   checkmate::assert_number(pthresh, lower = 0)
   checkmate::assert_number(select1, lower = 1, upper = length(data))
   checkmate::assert_number(select2, lower = 1, upper = length(data))
-  choices <- c("pval1", "pval2", "logFC1", "logFC2")
+  choices <- c("pval1", "pval2")
   checkmate::assert_choice(color_by, choices)
 
   # ---------------------------- data preparation -----------------------------
   data_red <- list(data[[select1]], data[[select2]])
 
   for(i in 1:length(data_red)) {
-    # data_red[i][[1]] <- subset(data_red[i][[1]], data_red[i][[1]]$pval < pthresh)
     colnames(data_red[i][[1]]) <- c(paste("logFC", i, sep=""),
                                     paste("pval", i, sep=""))
   }
