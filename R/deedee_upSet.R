7b5d6021f8f0de4419b1dcfe1fb79791d9030c16
Lea Rothörl <lea.rothoerl@th-bingen.de>
2021-07-29 12:08:18 +0200

    ComplexUpset instead of UpSetR

--- R/deedee_upSet.R
+++ R/deedee_upSet.R
@@ -20,7 +20,8 @@
 
 deedee_upSet <- function(data,
                          pthresh = 0.05,
-                         mode = "both") {
+                         mode = "both",
+                         min_setsize = 10) {
 
   # ----------------------------- argument check ------------------------------
   checkmate::assert_list(data, type = "data.frame", min.len = 2)
@@ -30,6 +31,7 @@ deedee_upSet <- function(data,
   checkmate::assert_number(pthresh, lower = 0)
   choices <- c("up", "down", "both", "both_colored")
   checkmate::assert_choice(mode, choices)
+  checkmate::assert_number(min_setsize, lower = 0)
 
   # ---------------------------- data preparation -----------------------------
   for(i in 1:length(data)) {
@@ -55,83 +57,77 @@ deedee_upSet <- function(data,
       data[i][[1]] <- subset(data[i][[1]], data[i][[1]]$logFC == -1)
     }
     data[i][[1]] <- tibble::rownames_to_column(data[i][[1]])
+    names(data[i][[1]]) <- c("rowname", names(data[i]))
   }
+
   comp <- dplyr::full_join(data[1][[1]], data[2][[1]], by = "rowname",
                              copy = FALSE)
+
   if (length(data) > 2) {
     for (i in 3:length(data)) {
       comp <- dplyr::full_join(comp, data[i][[1]], by = "rowname", copy = FALSE)
     }
   }
+  comp <- tibble::column_to_rownames(comp)
+  contrasts <- colnames(comp)
 
-  comp = (comp != 0 & !isNA(comp))
   # -------------------------------- coloring ---------------------------------
   if (mode == "both_colored") {
-    comp <- tibble::column_to_rownames(comp, "rowname")
-    count <- vector(mode = "numeric", length = length(comp["logFC.x"]))
+    count <- vector(mode = "numeric", length = length(comp[[1]]))
     comp <- cbind(comp, count)
     comp["count"] <- rowSums(comp, na.rm = TRUE)
 
-    na_count <- vector(mode = "numeric", length = length(comp[, "logFC.x"]))
+    na_count <- vector(mode = "numeric", length = length(comp[[1]]))
     comp <- cbind(comp, na_count)
     comp["na_count"] <- rowSums(is.na(comp))
 
-    all_up <- vector(mode = "logical", length = length(comp[, "logFC.x"]))
-    comp <- cbind(comp, all_up)
-    all_down <- vector(mode = "logical", length = length(comp[, "logFC.x"]))
-    comp <- cbind(comp, all_down)
+    dr <- vector(mode = "character", length = length(comp[[1]]))
+    comp <- cbind(comp, dr)
 
-    for (i in 1:length(comp[, "logFC.x"])) {
-      if ((comp[i, "count"] + comp[i, "na_count"]) == 4) {
-        comp[i, "all_up"] <- TRUE
+    nsets <- length(data)
+
+    for (i in 1:length(comp[[1]])) {
+      if ((comp[i, "count"] + comp[i, "na_count"]) == nsets) {
+        comp[i, "dr"] <- "up"
       }
-      else if ((comp[i, "count"] - comp[i, "na_count"]) == -4) {
-        comp[i, "all_down"] <- TRUE
+      else if ((comp[i, "count"] - comp[i, "na_count"]) == -nsets) {
+        comp[i, "dr"] <- "down"
+      }
+      else {
+        comp[i, "dr"] <- "different"
       }
     }
 
     comp <- subset(comp, select = -c(count, na_count))
-    comp[is.na(comp)] <- 0
-    comp[comp == -1] <- 1
-    colnames(comp) <- c(names(data), "all_up", "all_down")
-
-    same_dir <- vector(mode = "logical", length = length(comp[, "all_up"]))
-    comp <- cbind(comp, same_dir)
-
-    for (i in 1:length(comp[, "all_up"])) {
-      if (comp[i, "all_up"] == TRUE || comp[i, "all_down"] == TRUE) {
-        comp[i, "same_dir"] <- TRUE
-      }
-    }
-
-    for (i in 1:(length(data))) {
-      data[i][[1]] <- subset(data[i][[1]], select = "rowname")
-      colnames(data[i][[1]]) <- names(data)[i]
-      data[i] <- data[i][[1]]
-    }
+    comp[is.na(comp)] <- FALSE
+    comp[comp == -1 | comp == 1] <- TRUE
   }
 
   # ------------------ creation of the resulting UpSet plot -------------------
-  col_up = viridis::viridis(n = 1, begin = 0.4, option="magma")
-  col_down = viridis::viridis(n = 1, begin = 0.9, option="magma")
-
   if (mode == "both_colored") {
-    res <- UpSetR::upset(comp,
-                 order.by = "freq",
-                 nsets = length(data),
-                 queries = list(list(query = UpSetR::elements,
-                                     params = c("same_dir", TRUE),
-                                     color = col_up, active = TRUE,
-                                     query.name = "only positive logFCs"),
-                                list(query = UpSetR::elements,
-                                     params = list("all_down", TRUE),
-                                     color = col_down, active = TRUE,
-                                     query.name = "only negative logFCs")),
-                 query.legend = "top")
-
+    col_up = viridis::viridis(n = 1, begin = 0.4, option="magma")
+    col_down = viridis::viridis(n = 1, begin = 0.9, option="magma")
+    res <- ComplexUpset::upset(comp,
+            contrasts,
+            guide = NULL,
+            base_annotations = list(
+              "Intersection of genes" = ComplexUpset::intersection_size(
+                counts = FALSE,
+                mapping = ggplot2::aes(fill = dr))
+              + ggplot2::scale_fill_manual(values = c("different"= "dark grey",
+                                                      "down" = col_down,
+                                                      "up" = col_up),
+                                           name = "Regulation direction")
+                               ),
+            width_ratio = 0.1,
+            min_size = min_setsize)
   }
   else {
-    res <- ComplexUpset::upset(comp, names(comp)[2:length(names(comp))])
+    comp <- (comp != 0 & !is.na(comp))
+    comp <- as.data.frame(comp)
+    res <- ComplexUpset::upset(comp,
+                               contrasts,
+                               guide = NULL)
   }
 
   # --------------------------------- return ----------------------------------
