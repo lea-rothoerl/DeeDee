cea2dbf31f45bddae78279ff11b4b8248e9643c1
Lea Rothoerl <lea.rothoerl@th-bingen.de>
2021-10-05 12:59:29 +0200

    adding param ref and embedding in app

--- R/deedee_qq.R
+++ R/deedee_qq.R
@@ -40,7 +40,9 @@ deedee_qq <- function(data,
                       pthresh = 0.05,
                       select1 = 1,
                       select2 = 2,
-                      color_by = "pval1") {
+                      color_by = "pval1",
+                      multiple = FALSE,
+                      ref = 1) {
 
   # ----------------------------- argument check ------------------------------
   checkmate::assert_list(data, type = "data.frame", min.len = 2)
