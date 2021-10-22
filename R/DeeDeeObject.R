1ddd6a86cb1b03c7cc98ee7deeb1b644fa0b9a38
Lea Rothoerl <lea.rothoerl@th-bingen.de>
2021-10-22 13:37:45 +0200

    some aesthetic changes

--- R/DeeDeeObject.R
+++ R/DeeDeeObject.R
@@ -3,36 +3,36 @@ DeeDeeObject <- setClass(
   "DeeDeeObject",
 
   slots = c(
-    DEA_results = "list"
+    DeeDeeList = "list"
   ),
 
   prototype = list(
-    DEA_results = NULL
+    DeeDeeList = NULL
   ),
 
   validity = function(object) {
-    res <- object@DEA_results
+    res <- object@DeeDeeList
     if (class(res) != "list") {
-      return("DEA_results must be a list of DeeDee data.frames.")
+      return("DeeDeeList must be a list of DeeDee data.frames.")
     }
     for (i in 1:length(res)) {
       if (class(res[[i]]) != "data.frame") {
-        return("DEA_results must be a list of DeeDee data.frames.")
+        return("DeeDeeList must be a list of DeeDee data.frames.")
       }
       if (checkmate::test_subset(names(res[[i]]), c("logFC", "pval")) == FALSE) {
-        return("DEA_results must be a list of DeeDee data.frames.")
+        return("DeeDeeList must be a list of DeeDee data.frames.")
       }
       if (class(res[[i]]$logFC) != "numeric") {
-        return("LogFC values must be of type 'numeric'.")
+        return("LogFC values must be a vector of type 'numeric'.")
       }
       if (class(res[[i]]$pval) != "numeric") {
-        return("P-values must be of type 'numeric'.")
+        return("P-values must be a vector of type 'numeric'.")
       }
       if (max(res[[i]]$pval) > 1) {
-        return("P-value cannot be greater than 1.")
+        return("A p-value cannot be greater than 1.")
       }
       if (min(res[[i]]$pval) < 0) {
-        return("P-value cannot be smaller than 0.")
+        return("A p-value cannot be lower than 0.")
       }
     }
     return(TRUE)
