1fadcbb5fa45a4454f985328e0c7df4c3dbfa36a
Federico Marini <marinif@uni-mainz.de>
2022-11-08 11:59:35 +0100

    validity for the DeeDeeExperiment class

---
+++ R/validity.R
@@ -0,0 +1,36 @@
+validDeeDeeExperiment <- function(object) {
+  msg <- NULL
+
+  if (!is(object@dea, "list")) {
+    msg <- c(msg, "'dea' must be a list")
+  }
+
+  if (any(is.null(names(object@dea)))) {
+    msg <- c(msg, "'dea' must be a named list")
+  }
+
+  dea_names <- names(object@dea)
+
+  required_rowdata <- unlist(
+    lapply(
+      dea_names, function(arg)
+        c(
+          paste0(arg, "_log2FoldChange"),
+          paste0(arg, "_pvalue"),
+          paste0(arg, "_padj")
+        )
+    )
+  )
+  if (!all(required_rowdata %in% colnames(rowData(object)))) {
+    msg <- c(msg, "some required columns were not found in the rowData")
+  }
+
+  if (is.null(msg)) {
+    TRUE
+  } else msg
+
+
+}
+
+setValidity("DeeDeeExperiment", validDeeDeeExperiment)
+
