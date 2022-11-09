f6d92513dcde0c886f921d3d52136876e46cc153
Federico Marini <marinif@uni-mainz.de>
2022-11-09 23:05:58 +0100

    testing the summary report gets generated

---
+++ tests/testthat/test-DeeDee_misc.R
@@ -0,0 +1,23 @@
+
+test_that("summary report", {
+  dde <- DeeDeeExperiment(
+    se_macrophage_noassays,
+    de_results = del
+  )
+
+  temp_output_file <- "test_report.html"
+  ddedde_summary(
+    dde = dde,
+    template = system.file("extdata",
+                           "summary_template.Rmd",
+                           package = "DeeDee"),
+    output_path = temp_output_file
+  )
+
+  # browseURL(temp_output_file)
+  expect_true(file.exists(temp_output_file))
+
+  file.remove(temp_output_file)
+})
+
+
