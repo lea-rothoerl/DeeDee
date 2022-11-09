2a2d36ceeb1f1888637226ee076db48ef8d72f74
Federico Marini <marinif@uni-mainz.de>
2022-11-09 20:03:54 +0100

    setup tests for running shiny app

---
+++ tests/testthat/test-DeeDee_app.R
@@ -0,0 +1,7 @@
+test_that("shiny", {
+  data(dd_list_original, package = "DeeDee")
+
+  myapp <- ddedde_app(deedee_obj = DeeDeeLegacy::DeeDeeObject(DeeDeeList = dd_list_original))
+
+  expect_is(myapp, "shiny.appobj")
+})
