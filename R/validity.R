69277973c51ffc5129cebca59141549e9ab0e9eb
Federico Marini <marinif@uni-mainz.de>
2022-11-09 11:17:12 +0100

    adding condition to check for validity - at least there has to be a DE result entered

--- R/validity.R
+++ R/validity.R
@@ -5,24 +5,26 @@ validDeeDeeExperiment <- function(object) {
     msg <- c(msg, "'dea' must be a list")
   }
 
-  if (any(is.null(names(object@dea)))) {
-    msg <- c(msg, "'dea' must be a named list")
-  }
+  if (length(dea(object)) > 0) {
+    if (any(is.null(names(object@dea)))) {
+      msg <- c(msg, "'dea' must be a named list")
+    }
 
-  dea_names <- names(object@dea)
+    dea_names <- names(object@dea)
 
-  required_rowdata <- unlist(
-    lapply(
-      dea_names, function(arg)
-        c(
-          paste0(arg, "_log2FoldChange"),
-          paste0(arg, "_pvalue"),
-          paste0(arg, "_padj")
-        )
+    required_rowdata <- unlist(
+      lapply(
+        dea_names, function(arg)
+          c(
+            paste0(arg, "_log2FoldChange"),
+            paste0(arg, "_pvalue"),
+            paste0(arg, "_padj")
+          )
+      )
     )
-  )
-  if (!all(required_rowdata %in% colnames(rowData(object)))) {
-    msg <- c(msg, "some required columns were not found in the rowData")
+    if (!all(required_rowdata %in% colnames(rowData(object)))) {
+      msg <- c(msg, "some required columns were not found in the rowData")
+    }
   }
 
   if (is.null(msg)) {
