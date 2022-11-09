70b3170489ddc8ffa7d3a918df1950f5c325fbc4
Federico Marini <marinif@uni-mainz.de>
2022-11-09 15:14:50 +0100

    expanding the pool of generic & methods

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -5,3 +5,16 @@ setGeneric("dea", function(x, ...) standardGeneric("dea"))
 
 #' @export
 setGeneric("dea<-", function(x, value) standardGeneric("dea<-"))
+
+#' @export
+setGeneric("add_dea", function(x, dea, ...) standardGeneric("add_dea"))
+
+#' @export
+setGeneric("remove_dea", function(x, dea_name, ...) standardGeneric("remove_dea"))
+
+#' @export
+setGeneric("get_dea_df", function(x, dea_name, ...) standardGeneric("get_dea_df"))
+
+#' @export
+setGeneric("get_dea_list", function(x, ...) standardGeneric("get_dea_list"))
+
