82c1bc5330d8a5f088fa3706d97f2ea211aa66d6
Federico Marini <marinif@uni-mainz.de>
2022-11-09 11:03:08 +0100

    adding some basic unit tests for creation

---
+++ tests/testthat/test-DeeDeeExperiment.R
@@ -0,0 +1,37 @@
+test_that("creating", {
+  dde <- DeeDeeExperiment(
+    se_macrophage_noassays,
+    de_results = del
+  )
+  dde
+
+  expect_is(dde, "DeeDeeExperiment")
+  new_del <- list(
+    ifng2 = del$ifng_vs_naive,
+    ifngsalmo2 = del$ifngsalmo_vs_naive
+  )
+  # add a new (set of) DE result(s)
+  dde_new <- add_dea(dde, new_del)
+})
+
+
+test_that("adding and removing", {
+  dde <- DeeDeeExperiment(
+    se_macrophage_noassays,
+    de_results = del
+  )
+
+  new_del <- list(
+    ifng2 = del$ifng_vs_naive,
+    ifngsalmo2 = del$ifngsalmo_vs_naive
+  )
+  # add a new (set of) DE result(s)
+  dde_new <- add_dea(dde, new_del)
+  expect_is(dde_new, "DeeDeeExperiment")
+  expect_equal(length(dea(dde)), 4)
+  expect_equal(length(dea(dde_new)), 6)
+
+  dde_removed <- remove_dea(dde, "ifngsalmo_vs_naive")
+  expect_is(dde_removed, "DeeDeeExperiment")
+  expect_equal(length(dea(dde_removed)), 3)
+})
