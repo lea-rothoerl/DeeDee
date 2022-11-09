5d965d36d83ea2a68afc408b0101be687a70a03f
Federico Marini <marinif@uni-mainz.de>
2022-11-09 19:37:41 +0100

    basic set of testing for the plotting functionality

---
+++ tests/testthat/test-DeeDee_plots.R
@@ -0,0 +1,36 @@
+dde <- DeeDeeExperiment(
+  se_macrophage_noassays,
+  de_results = del
+)
+
+test_that("plotting & plotting" {
+  p_scatter <- ddedde_scatter(dde)
+  expect_is(p_scatter, "gg")
+
+
+  p_heatmap <- ddedde_heatmap(dde)
+  expect_is(p_heatmap, "Heatmap")
+
+
+  p_venn <- ddedde_venn(dde)
+  expect_is(p_venn, "gg")
+
+
+  p_upset <- ddedde_upset(dde)
+  expect_is(p_upset, "gg")
+
+
+  p_cat <- ddedde_cat(dde)
+  expect_is(p_cat, "gg")
+
+
+  p_qq <- ddedde_qq(dde)
+  expect_is(p_qq, "gg")
+  p_qq_m <- ddedde_qqmult(dde)
+  expect_is(p_qq_m, "gg")
+
+
+  # ddedde_summary(dde)
+})
+
+
