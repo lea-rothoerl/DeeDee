21091b70a5ca550e0a780e8fb8b2215283e6c9b8
Federico Marini <marinif@uni-mainz.de>
2023-01-05 14:10:47 +0100

    adding example for creating dde objects

--- R/DeeDeeExperiment.R
+++ R/DeeDeeExperiment.R
@@ -13,7 +13,27 @@
 #' @export
 #'
 #' @examples
-#' # todo
+#' data("de_named_list", package = "DeeDee")
+#'
+#' dde_onlyde <- DeeDeeExperiment(
+#'   de_results = del
+#' )
+#'
+#' # or, with a SE object as support - even without assay data available
+#' library("SummarizedExperiment")
+#'
+#' rd_macrophage <- DataFrame(
+#'   gene_id = rownames(del$ifng_vs_naive))
+#' rownames(rd_macrophage) <- rownames(del$ifng_vs_naive)
+#' se_macrophage_noassays <- SummarizedExperiment(
+#'   assays = SimpleList(),
+#'   rowData = rd_macrophage
+#' )
+#'
+#' dde <- DeeDeeExperiment(
+#'   se_macrophage_noassays,
+#'   de_results = del
+#' )
 DeeDeeExperiment <- function(se = NULL,
                              de_results = NULL) {
 
