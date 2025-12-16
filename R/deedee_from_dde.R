a1d09e5dea39c02d5d4a49dd106d7c493f7fa362
Lea Schwarz <lea.rothoerl@posteo.de>
2025-12-16 12:08:41 +0100

    add function to generate DeeDee-compliant input from DeeDeeExperiment object

---
+++ R/deedee_from_dde.R
@@ -0,0 +1,35 @@
+#' DeeDee Prepare
+#'
+#' @description `deedee_from_dde` prepares an instance of the DeeDeeExperiment
+#' class for the usage in other DeeDee functions.
+#'
+#' @param dde Instance of the DeeDeeExperiment class
+#'
+#' @return list of DeeDee tables, to be used as part of the input for the other
+#' DeeDee functions
+#'
+#' @examples
+#'
+#' deedee_table <- deedee_from_dde(dde)
+#'
+
+deedee_from_dde <- function(dde) {
+  dea_names <- names(dde@dea)
+
+  deedee_list <- lapply(dea_names, function(name) {
+    res <- getDEA(dde, name)
+    df <- data.frame(
+      logFC = res[[1]],
+      pval  = res[[2]],
+      row.names = rownames(res)
+    )
+    df$logFC <- as.numeric(df$logFC)
+    df$pval  <- as.numeric(df$pval)
+    df
+  })
+
+  names(deedee_list) <- dea_names
+  deedee_list
+}
+
+deedee_obj_limma <- deedee_from_dde(dde_limma)
