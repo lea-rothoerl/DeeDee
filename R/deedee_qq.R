634c92d2371446d39d205f7612a39bb26cd89559
lea-rothoerl <lea.rothoerl@posteo.de>
2026-08-03 10:30:20 +0200

    check() doesn't find deedee_qq()??

--- R/deedee_qq.R
+++ R/deedee_qq.R
@@ -20,6 +20,8 @@
 #' data(dde_macrophage, package = "DeeDee")
 #' deedee_qq(dde_macrophage, select1 = 1, select2 = 2)
 #'
+#' @export
+#'
 
 deedee_qq <- function(data,
                       select1 = 1,
